45e249abd2b95cb540369cf58ae30ad9e6d730fe
Myles C. Maxfield <mmaxfield@apple.com>
2015-08-02 20:19:59 -0700

    Compute Pipeline State uniquing

--- MetalEditor/ComputePipelineState.swift
+++ MetalEditor/ComputePipelineState.swift
@@ -10,7 +10,39 @@ import Foundation
 import CoreData
 
 class ComputePipelineState: NSManagedObject {
+    private func validatePropertyIsUnique(name: String, value: AnyObject) throws {
+        guard let context = managedObjectContext else {
+            return
+        }
 
-// Insert code here to add functionality to your managed object subclass
+        let fetchRequest = NSFetchRequest(entityName: "ComputePipelineState")
+        fetchRequest.predicate = NSPredicate(format: "%K = %@", argumentArray: [name, value])
+        var states: [ComputePipelineState] = []
+        do {
+            states = try context.executeFetchRequest(fetchRequest) as! [ComputePipelineState]
+        } catch {
+        }
+        for state in states {
+            if state != self {
+                throw NSError(domain: "MetalEditorComputePipelineState", code: NSManagedObjectValidationError, userInfo: nil)
+            }
+        }
+    }
+
+    private func validateUnique() throws {
+        try validatePropertyIsUnique("name", value: name)
+        try validatePropertyIsUnique("id", value: id)
+    }
+    
+
+    override func validateForInsert() throws {
+        try super.validateForInsert()
+        try validateUnique()
+    }
+
+    override func validateForUpdate() throws {
+        try super.validateForUpdate()
+        try validateUnique()
+    }
 
 }
